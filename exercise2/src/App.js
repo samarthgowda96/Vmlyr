c5431d2aba263fbf39b88d6dc837330744697586
Samarth Gowda <samarthgowda@samarths-mbp.lan>
2021-06-29 14:41:36 -0700

    excercise2 project initialization

---
+++ exercise2/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>hellos</h1>
+    </div>
+  );
+}
+
+export default App;

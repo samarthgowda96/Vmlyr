d78b118115cc1050da84f6c0d2e2b5213d4f48d6
Samarth Gowda <samarthgowda@samarths-mbp.lan>
2021-06-29 15:24:46 -0700

    created navbar

--- exercise2/src/App.js
+++ exercise2/src/App.js
@@ -1,11 +1,27 @@
-
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Nav,Navbar,Container} from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-     <h1>hellos</h1>
+  
+     <Nav fill defaultActiveKey="/home" className="navbar" variant="dark" bg="dark" >
+      <Nav.Item className="link" variant="dark" bg="dark">
+        <Nav.Link href="/home" className="link" variant="dark" bg="dark">Home</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/aboutus" className="link">About Us</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/Adopt" className="link">Adopt</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/Kittens" className="link"> Kittens</Nav.Link>
+      </Nav.Item>
+    </Nav>
+    <Container
+    
     </div>
   );
 }

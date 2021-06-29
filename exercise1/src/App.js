6a443cad6132e9821ce994887f1bfdff55693adc
Samarth Gowda <samarthgowda@samarths-mbp.lan>
2021-06-29 12:44:32 -0700

    Created Header

--- exercise1/src/App.js
+++ exercise1/src/App.js
@@ -1,10 +1,17 @@
-
-import './App.css';
-
-function App() {
-  return (
-    <h1>Excercise 1</h1>
-  );
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Badge from 'react-bootstrap/Badge'
+import Container from 'react-bootstrap/Container'
+const App=()=>{
+    return(
+        <div>
+            <Container className="header-cont">
+                <h1 className="header-name">
+                    <Badge 
+                    style={{textAlign:"center", color:"darkblue"}}>Hello there </Badge>
+                </h1>
+            </Container>
+        </div>
+    )
 }
-
 export default App;

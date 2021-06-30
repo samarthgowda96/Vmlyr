4a4347c3d053fdc86305257d7ba246aaa55ec1e5
Samarth Gowda <samarthgowda@samarths-mbp.lan>
2021-06-29 17:03:28 -0700

    completed the assessment

--- exercise2/src/App.js
+++ exercise2/src/App.js
@@ -31,16 +31,26 @@ function App() {
             <div class="flexbox-item flexbox-item-1">
               <h2 className="flexbox-titles">What We Do</h2>
               <div className="images">
-                  <img src=" https://images-na.ssl-images-amazon.com/images/I/51Ej2xA7CkL.jpg" alt="kittens pic" width="350px" height="200px"/>
+                  <img src=" https://kittenrescue.org/wp-content/uploads/2015/01/KittenRescue_FB3.jpg" alt="kittens pic" width="380px" height="200px"/>
               </div>
+              <p class="flexbox-title-sub">We are a charity organization that takes in rescue kittens and give them to owners.<a href="/more" className="flexbox-title-sub">More...</a></p>
             </div>
             <div class="flexbox-item flexbox-item-2">
             <h2 className="flexbox-titles">Our Kitties</h2>
               <div className="images">
-                  <img src=" https://images-na.ssl-images-amazon.com/images/I/51Ej2xA7CkL.jpg" alt="kittens pic" width="350px" height="200px"/>
+                  <img src="https://www.americanhumane.org/app/uploads/2016/08/animals-cats-cute-45170-min-1024x569.jpg" alt="kittens pic" width="380px" height="200px"/>
               </div>
+              <p class="flexbox-title-sub">Our Kitties are given proper shots and are cleaned up for their new homes, <a href="/more" className="flexbox-title-sub">More...</a></p>
             </div>
-            <div class="flexbox-item flexbox-item-3">What You Can Do</div>
+            <div class="flexbox-item flexbox-item-3">
+            
+            <h2 className="flexbox-titles">What You Can Do</h2>
+              <div className="images">
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZdPR4hcDJlNKhrKNonet2OqgKC58UqjnZlA&usqp=CAU" alt="kittens pic" width="380px" height="200px"/>
+              </div>
+              <p class="flexbox-title-sub">Take in a rescue kitten! We need more owners who are willing to take one home with them.<a href="/more" className="flexbox-title-sub">More...</a></p>
+
+            </div>
 
       </div>
     </div>
